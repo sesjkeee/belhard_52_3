508340bd90e77cd6196760eae7cc556c55583525
sesjkeee <slavnet-minsk@inbox.ru>
2022-04-09 23:35:49 +0300

    initial commit

--- lesson3_3_2.py
+++ lesson3_3_2.py
@@ -1,3 +1,6 @@
 name = input()
 age = int(input())
 city = input()
+'Добрый день {}. Ваш возвраст {} лет. Вы проживаете в городе {}'.format(name, age, city)
+print('Добрый день {}. Ваш возвраст {} лет. Вы проживаете в городе {}'.format(name, age, city))
+
