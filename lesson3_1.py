b144ed599b5485266bb8181497dd3d223969e55b
sesjkeee <slavnet-minsk@inbox.ru>
2022-04-09 17:21:29 +0300

    initial commit # add print
    # add branches

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,3 +1,3 @@
-enter_the_text = input('Ввести текст из нескольких слов разделеных -')
+enter_the_text = input('Ввести текст из нескольких слов разделенных тире')
 enter_the_text = enter_the_text.replace('-', ' ')
 print(enter_the_text)
